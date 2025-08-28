b8f13ef6172b0e2a9e5d46461e6175f6528c298c
yooooobook <146624730+yooooobook@users.noreply.github.com>
2025-08-28 14:22:02 +0800

    調整信箱驗證頁面

--- pages/user/verifyEmail.vue
+++ pages/user/verifyEmail.vue
@@ -21,7 +21,7 @@
             </div>
         </Dialog>
 
-        <template v-if="emailVerified">
+        <template v-if="data">
             <div class="text-center">
                 <h1 class="mb-24">信箱驗證完成</h1>
                 <NuxtLink
@@ -32,73 +32,27 @@
             </div>
         </template>
         <template v-else>
-            <h1 class="mb-24">請再次驗證您的電子郵件地址</h1>
-            <p class="mb-16">{{ verifyfailMessage }}</p>
-            <div
-                @click="sendEmailVerify"
-                class="bg-secondary hover:bg-[#d80545] px-16 py-12 text-white font-bold cursor-pointer inline-block"
-            >
-                重發驗證信
-            </div>
+            <h1 class="mb-24">信箱驗證失敗</h1>
+            <p class="mb-16">{{ mapErrorMessage(error.data?.message, error.data?.statusCode) }}</p>
         </template>
     </div>
 </template>
 
 <script setup>
-const { $api } = useNuxtApp()
 const { oobCode } = useRoute().query
-const userStore = useUserStore()
-const { idToken, emailVerified } = storeToRefs(userStore)
-const { notify } = useToastifyStore()
 const isDialogVisible = ref(false)
-const verifyfailMessage = ref('')
 
-const confirmAndSaveEmailVerification = async () => {
-    if (emailVerified.value) return
-    if (!oobCode) return
-
-    try {
-        await $fetch(apiList.member.confirmEmailVerification.serverPath, {
+const { data, error } = useAsyncData('emailVerification', () => {
+    if (oobCode) {
+        return $fetch(apiList.member.confirmEmailVerification.serverPath, {
             method: apiList.member.confirmEmailVerification.method,
             body: {
                 oobCode
             }
         })
-    } catch (e) {
-        verifyfailMessage.value = mapErrorMessage(e.data?.message, e.data?.statusCode)
+    } else {
+        navigateTo('/')
     }
-}
-
-const sendEmailVerify = async () => {
-    const emailVerify = apiList.member.sendEmailVerify
-    try {
-        await $api(emailVerify.serverPath, {
-            method: emailVerify.method,
-            body: {
-                idToken: idToken.value
-            }
-        })
-        isDialogVisible.value = true
-    } catch (e) {
-        if (e.statusCode === 400 && e.message === 'INVALID_ID_TOKEN') {
-            useCookie('userName').value = null
-            useCookie('email').value = null
-            useCookie('emailVerified').value = null
-            useCookie('photoUrl').value = null
-            useCookie('userId').value = null
-            useCookie('idToken').value = null
-            useCookie('refreshToken').value = null
-            useCookie('signInProvider').value = null
-            userStore.$reset()
-            navigateTo('/user/login')
-        } else {
-            notify('error', e.message, e.statusCode)
-        }
-    }
-}
-
-onMounted(() => {
-    confirmAndSaveEmailVerification()
 })
 </script>
 
